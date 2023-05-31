d522ea71110e68441339645f04792405c4cad211
NTonakanyan <narekt@local.express>
2023-05-31 16:14:18 +0400

    add new parameter FINGERPRINT and update gradle

--- exceptionlibrary/src/main/java/com/android/exceptionlibrary/ExceptionHandler.kt
+++ exceptionlibrary/src/main/java/com/android/exceptionlibrary/ExceptionHandler.kt
@@ -36,7 +36,7 @@ internal object ExceptionHandler {
                     "deviceModel -> ${Build.MODEL}\n" +
                     "versionName -> ${pair.first}\n" +
                     "versionCode -> ${pair.second}\n" +
-                    "deviceModel -> ${Build.MODEL}\n" +
+                    "fingerPrint -> ${Build.FINGERPRINT}\n" +
                     "androidVersionCode -> ${Build.VERSION.SDK_INT}\n" +
                     "deviceId -> ${Settings.Secure.getString(mApplication.contentResolver, Settings.Secure.ANDROID_ID)}\n" +
                     "threadName -> ${thread.name}\n" +
